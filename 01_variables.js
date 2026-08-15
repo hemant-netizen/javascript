4209826f0ed3565f85cc4e7723d890d282a947fb
hemant-netizen <hemantraj595@gmail.com>
2026-08-15 16:02:41 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "Hemant@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+accountEmail = "hcf@hc.com"
+accountPassword = "1212421"
+accountCity = "Patna"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = 33
+let value = 5 
+let negvalue = -value
+console.log(negvalue);
+console.log("1" + 2);
+console.log(1+"2");
+console.log(1 + 2 + "2");
+console.log((3+4) * 5 % 3);
+
+
+
+
+
