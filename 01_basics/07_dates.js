b8248922db2f2d824258399369f5b26f27e6d9a1
hemant-netizen <hemantraj595@gmail.com>
2026-08-16 19:17:09 +0530

    review agian

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,15 @@
+ let mydate = new Date()
+ console.log(mydate.toString());
+ console.log(mydate.toISOString());
+ console.log(typeof mydate);
+ console.log(mydate.toDateString());
+ console.log(mydate.toLocaleDateString());
+ 
+
+ let myTimeStamp = Date.now();
+ console.log(myTimeStamp);
+ 
+ 
+ 
+ 
+ 
