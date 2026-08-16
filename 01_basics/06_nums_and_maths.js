f9694d0b3d0d2bc4ca89158b1299cfa240db37bd
hemant-netizen <hemantraj595@gmail.com>
2026-08-16 18:56:16 +0530

    update

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,6 +1,16 @@
-const score = 100
-console.log(score);
-const balance = new Number(400)
-console.log(balance);
+//const score = 100
+//console.log(score);
+//const balance = new Number(400)
+//console.log(balance);
+//console.log(Math.random());
+//console.log(Math.random() *10 +1);
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
 
 
