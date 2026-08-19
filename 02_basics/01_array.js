9fa3356e2aa50aedc63050b52dbb3c63e52ca20c
hemant-netizen <hemantraj595@gmail.com>
2026-08-20 00:41:06 +0530

    array_basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,10 @@
+const myarr = [0, 2, 3, 4, 5]
+const myHeros = ["Shaktiman", "Heropanti"]
+const myarr2 = new Array(1, 2, 3, 4)
+console.log(myarr2);
+
+console.log(myarr);
+
+// Array methods
+myarr.push(6)
+console.log(myarr);
